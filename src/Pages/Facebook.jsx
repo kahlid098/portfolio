59da4404e5b8feb1a42da3fad35144a286843f38
khalid098 <ka1239996@gmail.com>
2024-06-07 01:28:48 +0530

    done

--- src/Pages/Facebook.jsx
+++ src/Pages/Facebook.jsx
@@ -1,33 +1,21 @@
-import React, { useEffect } from 'react';
-import '../Pages/facesbook.css'
+import React from "react";
+import "../Pages/facesbook.css";
 
 const FacebookPageEmbed = () => {
-  useEffect(() => {
-    if (window.FB) {
-      window.FB.XFBML.parse();
-    }
-  }, []);
+
 
   return (
     <div>
-      <div
-        className="fb-page"
-        data-href="https://www.facebook.com/FahimShakirWebsiteDeveloper/"
-        data-tabs="timeline"
-        data-width="500"
-        data-height="310"
-        data-small-header="false"
-        data-adapt-container-width="true"
-        data-hide-cover="false"
-        data-show-facepile="true"
-      >
-        <blockquote
-          cite="https://www.facebook.com/facebook"
-          className="fb-xfbml-parse-ignore"
-        >
-          <a href="https://www.facebook.com/facebook">Facebook</a>
-        </blockquote>
-      </div>
+     
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFahimShakirWebsiteDeveloper%2F%3Fref%3Dembed_page&amp;tabs=timeline&amp;width=340&amp;height=500&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId"
+        width="300"
+        height="300"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      ></iframe>
     </div>
   );
 };
