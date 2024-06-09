1ab148fcb41dc3540ff901a324c7026ba8ab9d07
khalid098 <ka1239996@gmail.com>
2024-06-10 02:18:40 +0530

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Header/Header.css";
 import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
@@ -11,97 +11,186 @@ import { GoMail } from "react-icons/go";
 import { FaSkype } from "react-icons/fa";
 import { IoIosCall } from "react-icons/io";
 import { FaWhatsapp } from "react-icons/fa";
-import logo from '../image/fs-logo.png'
+import logo from "../image/fs-logo.png";
 import { NavLink } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import { FaXmark } from "react-icons/fa6";
 
 const Header = () => {
-  const [isActive, setActive] = useState (false);
-  
-  const handleClick = ()=>{
-    setActive (true);
-  }
+  const [isSticky, setIsSticky] = useState(false);
+  const [isActive, setActive] = useState(false);
 
-  const closenave =()=>{
-    setActive(false)
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.pageYOffset;
+      if (offset > 180) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    setActive(true);
+  };
+
+  const closenave = () => {
+    setActive(false);
+  };
+
+  const navbarClass = isSticky ? "nav-main sticky" : "nav-main";
 
   return (
     <>
       <div className="bg-color">
         <div className="container4">
-
           <div className="d-flex">
             <div className="icon">
-            <a href="https://www.facebook.com/FahimShakirWebsiteDeveloper/" target="_blank" rel="noopener noreferrer">
-              <FaFacebookF className="icon-color" />
-            </a>
-            <a href="https://x.com/i/flow/login?redirect_after_login=%2Ffahimshakir786" target="_blank" rel="noopener noreferrer">
-              <FaXTwitter className="icon-color" />
-            </a>
-            <a href="https://in.pinterest.com/fahimwebdeveloper/_saved/" target="_blank" rel="noopener noreferrer" >
-              <FaPinterest className="icon-color" />
-            </a>
-            <a href="https://www.instagram.com/fahimwebdeveloper/" target="_blank" rel="noopener noreferrer">
-              <ImInstagram className="icon-color" />
-            </a>
-            <a href="https://www.linkedin.com/" className="icon-link" target="_blank" rel="noopener noreferrer" >
-              <FaLinkedinIn className="icon-color" />
-            </a>
-            <a href="https://github.com/fahim-web-developer" className="facebook-icon" target="_blank" rel="noopener noreferrer">
-              <VscGithub className="icon-color" />
-            </a>
-            <a href="https://play.google.com/store/apps/details?id=com.fahimshakir2" className="icon-link" target="_blank" rel="noopener noreferrer">
-              <BsAndroid2 className="icon-color" />
-            </a>
+              <a
+                href="https://www.facebook.com/FahimShakirWebsiteDeveloper/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebookF className="icon-color" />
+              </a>
+              <a
+                href="https://x.com/i/flow/login?redirect_after_login=%2Ffahimshakir786"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaXTwitter className="icon-color" />
+              </a>
+              <a
+                href="https://in.pinterest.com/fahimwebdeveloper/_saved/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaPinterest className="icon-color" />
+              </a>
+              <a
+                href="https://www.instagram.com/fahimwebdeveloper/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ImInstagram className="icon-color" />
+              </a>
+              <a
+                href="https://www.linkedin.com/"
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn className="icon-color" />
+              </a>
+              <a
+                href="https://github.com/fahim-web-developer"
+                className="facebook-icon"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <VscGithub className="icon-color" />
+              </a>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.fahimshakir2"
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BsAndroid2 className="icon-color" />
+              </a>
             </div>
 
-       <div className="sociel-icon">
-          <a href="https://github.com" className="icon-link" target="_blank" rel="noopener noreferrer">
-          <span  className="border"> <GoMail  className="information"/> fahimshakir786@gmail.com</span>
-          </a>
+            <div className="sociel-icon">
+              <a
+                href="https://github.com"
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="border">
+                  {" "}
+                  <GoMail className="information" /> fahimshakir786@gmail.com
+                </span>
+              </a>
 
-          <a href="https://github.com" className="icon-link" target="_blank" rel="noopener noreferrer">
-            <span className="border"><FaSkype className="information"/> fahim.shakir40</span>
-          </a>
+              <a
+                href="https://github.com"
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="border">
+                  <FaSkype className="information" /> fahim.shakir40
+                </span>
+              </a>
 
-          <a href="https://github.com" className="icon-link" target="_blank" rel="noopener noreferrer">
-            <span className="border"><IoIosCall className="information"/> 09718925775</span>
-          </a>
+              <a
+                href="https://github.com"
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="border">
+                  <IoIosCall className="information" /> 09718925775
+                </span>
+              </a>
 
-          <a href="https://github.com" className="icon-link" target="_blank" rel="noopener noreferrer">
-            <span ><FaWhatsapp  className="information"/> 09268751849</span>
-          </a>
+              <a
+                href="https://github.com"
+                className="icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>
+                  <FaWhatsapp className="information" /> 09268751849
+                </span>
+              </a>
             </div>
           </div>
         </div>
       </div>
 
-
-    <div className="bg-color">
-      <div className="container4">
-      <div className="d-flex">
-          <img src={logo} alt="Logo" className="image"/>
-          <FaBars  className="bar" onClick={handleClick}/>
+      <div className="bg-color">
+        <div className={`container4 ${navbarClass}`}>
+          <div className="d-flex">
+            <img src={logo} alt="Logo" className="image" />
+            <FaBars className="bar" onClick={handleClick} />
             <div>
-              <ul className={ isActive ? "ul navactive" : "ul"}>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/About">About Us</NavLink> </li>
-                <li><NavLink to="/Resume">Resume</NavLink> </li>
-                <li><NavLink to="/Service"> Our Sevices</NavLink></li>
-                <li><NavLink to="/Portfolio"> Our Portfolio</NavLink></li>
-                <li><NavLink to="/Blog"> Blog</NavLink></li>
-                <FaXmark  className="xmark" onClick={closenave}/>
-                <button className="contact-btn"><NavLink to="/contact">CONTACT US</NavLink></button>
+              <ul className={isActive ? "ul navactive" : "ul"}>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/About">About Us</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/Resume">Resume</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/Service">Our Services</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/Portfolio">Our Portfolio</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/Blog">Blog</NavLink>
+                </li>
+                <FaXmark className="xmark" onClick={closenave} />
+                <button className="contact-btn">
+                  <NavLink to="/contact">CONTACT US</NavLink>
+                </button>
               </ul>
             </div>
           </div>
+        </div>
       </div>
-    </div>
-     
-
-
     </>
   );
 };
